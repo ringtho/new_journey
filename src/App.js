aeba8665fa018147bfa32ba9223bd4c4ef9bf781
Smith Ringtho <sringtho@gmail.com>
2023-07-24 22:58:21 +0300

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import ImageSlider from './components/ImageSlider/ImageSlider'
-import { ImageData } from './components/ImageSlider/imageData'
+// import ImageSlider from './components/ImageSlider/ImageSlider'
+// import { ImageData } from './components/ImageSlider/imageData'
 
 function App () {
   return (
     <div className="App">
-      <Header />
-      <ImageSlider ImageData={ImageData} SlideInterValTime={5000}/>
+      <div className='home__wrapper'>
+        <Header />
+        {/* <ImageSlider ImageData={ImageData} SlideInterValTime={5000}/> */}
+      </div>
     </div>
   )
 }
